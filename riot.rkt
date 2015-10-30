5260df81652a5d8234bfd68f96320efdc74347db
Brice Ayres <briceayres@gmail.com>
2015-10-30 04:30:09 -0400

    draws lines, but won't draw vert and horizontal

--- riot.rkt
+++ riot.rkt
@@ -40,9 +40,60 @@
 (define (to-image w)
   (cond
     [(empty? w)
-     BACKGROUND (draw-columns)]
+     BACKGROUND
+     (draw-columns)(draw-rows)
+     ]
     [else (place-image BALLOON XSHOTS (first w) (to-image (rest w)))]))
 
 ; Draw columns
 (define (draw-columns)
-  (add-line BACKGROUND 10 0 10 180 "black"))
+  (add-line
+   (add-line
+    (add-line
+     (add-line
+      (add-line
+       (add-line
+        (add-line BACKGROUND 70 0 70 180 "black")
+        60 0 60 180 "black")
+       50 0 50 180 "black")
+      40 0 40 180 "black")
+     30 0 30 180 "black")
+    20 0 20 180 "black")
+   10 0 10 180 "black")
+  )
+
+; Draw rows
+(define (draw-rows)
+  (add-line
+   (add-line
+    (add-line
+     (add-line
+      (add-line
+       (add-line
+        (add-line
+         (add-line
+          (add-line
+           (add-line
+            (add-line
+             (add-line
+              (add-line
+               (add-line
+                (add-line
+                 (add-line
+                  (add-line BACKGROUND 0 10 80 10 "black")
+                  0 20 80 20 "black")
+                 0 30 80 30 "black")
+                0 40 80 40 "black")
+               0 50 80 50 "black")
+              0 60 80 60 "black")
+             0 70 80 70 "black")
+            0 80 80 80 "black")
+           0 90 80 90 "black")
+          0 100 80 100 "black")
+         0 110 80 110 "black")
+        0 120 80 120 "black")
+       0 130 80 130 "black")
+      0 140 80 140 "black")
+     0 150 80 150 "black")
+    0 160 80 160 "black")
+   0 170 80 170 "black"))

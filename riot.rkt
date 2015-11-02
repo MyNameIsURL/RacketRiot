2e6c7b18ada08d921453c5574e08a033e6ebe033
Ike Clinton <IkeClinton@gmail.com>
2015-11-02 09:10:51 -0500

    Moved random so that it can be used globally

--- riot.rkt
+++ riot.rkt
@@ -10,6 +10,10 @@
 ; graphical constants 
 (define BACKGROUND (empty-scene WIDTH HEIGHT))
 (define BALLOON (circle 3 "solid" "red"))
+
+; Random values
+(define randomX (random (/ WIDTH 10)))
+(define randomY (random (/ HEIGHT 10)))
  
 ; A ShotWorld is List-of-numbers. 
 ; interpretation the collection of shots fired and moving straight up
@@ -28,14 +32,12 @@
 ;    [(empty? w) '()]
 ;    [else (cons (sub1 (first w)) (tock (rest w)))]))
 (define (tock w)
-  (define randomX (random (/ WIDTH 10)))
-  (define randomY (random (/ HEIGHT 10)))
   (cond
     [(empty? w) '()]
     [else (place-image/align BALLOON (* 10 randomX) (* 10 randomY) "center" "center" BACKGROUND)]))
  
 ; ShotWorld KeyEvent -> ShotWorld 
-; adds a shot to the world if the space bar was hit 
+; adds a balloon to the world if the space bar was hit 
 (define (keyh w ke)
   (cond
     [(key=? ke " ") (cons HEIGHT w)]
